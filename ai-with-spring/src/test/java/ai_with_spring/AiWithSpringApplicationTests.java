5927b4ddcc227ba370549601ff30c00ccb1b1aa9
Nikhil13k <karankotnikhilkumar@gmail.com>
2025-05-21 10:31:01 +0530

    Initail Setup

---
+++ ai-with-spring/src/test/java/ai_with_spring/AiWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ai_with_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
